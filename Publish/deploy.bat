3bf27af2a9b7c56322eb2488bfba442c7c9f43a8
chriswil <chriswil@codev.net>
2024-08-18 10:28:39 -0600

    Remove old deprecated nuget server reference

--- Publish/deploy.bat
+++ Publish/deploy.bat
@@ -4,7 +4,7 @@
 
 for %%X IN (Asset,Banking,Cache,Cipher,Common,Communication,Export,Licensing,Task) DO (
   @echo Processing Codev.Core.%%X
-  @tools\nuget.exe push nupkg/Codev.Core.%%X.%1.nupkg perkwil42 -Timeout 600 -Source http://package.codev.net/nuget
+  @tools\nuget.exe push nupkg/Codev.Core.%%X.%1.nupkg %GITHUB_NUGET_TOKEN% -Source https://nuget.pkg.github.com/codev-software-llc/index.json
 )
 
 goto Done
