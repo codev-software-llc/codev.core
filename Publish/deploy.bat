2ae4afea991d9f4efbcd798e07434b1ccd508126
chriswil <chriswil@codev.net>
2025-12-08 17:49:34 -0700

    Nuget transition

--- Publish/deploy.bat
+++ Publish/deploy.bat
@@ -2,8 +2,10 @@
 
 @if "%1" == "" goto Usage
 
-@echo Processing Codev.Core
-@tools\nuget.exe push nupkg/Codev.Core.%1.nupkg %GITHUB_NUGET_TOKEN% -Source https://nuget.pkg.github.com/codev-software-llc/index.json
+@echo Deploying [Codev.Core.%1.nupkg] ... %CODEV_NUGET_PACKAGE_LOCATION%
+@copy /y nupkg\Codev.Core.%1.nupkg %CODEV_NUGET_PACKAGE_LOCATION%
+
+rem @tools\nuget.exe push nupkg/Codev.Core.%1.nupkg %GITHUB_NUGET_TOKEN% -Source https://nuget.pkg.github.com/codev-software-llc/index.json
 
 goto Done
 
